a95eced6546d472fd33a58a005868a06c44bb9ca
vrushal <vrushalnedungadi@gmail.com>
2024-12-12 20:20:23 -0800

    changes

--- SecurityWebhook.Lib.Models/Constants/VCSConstants.cs
+++ SecurityWebhook.Lib.Models/Constants/VCSConstants.cs
@@ -3,6 +3,6 @@
     public class VCSConstants
     {
         public const string Github = "Github";
-        public const string GithubToken = "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";
+        public const string GithubToken = "";
     }
 }
